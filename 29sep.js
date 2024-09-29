11252045cd5f194839765605bfac4b4d496d8fe0
ShanzayEman <shanzayeman0@gmail.com>
2024-09-29 20:42:01 +0500

    Add files via upload

---
+++ 29sep.js
@@ -0,0 +1,12 @@
+// sort by alphabetics
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.sort();
+//reverse method used to revsrevse the element in array 
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits1.reverse();
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const sorted = months.toSorted();
+document.getElementById("demo2").innerHTML = sorted;
+const months1 = ["Jan", "Feb", "Mar", "Apr"];
+const reversed = months1.toReversed();
+document.getElementById("demo3").innerHTML = reversed;
